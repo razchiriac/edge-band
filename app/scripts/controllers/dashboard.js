1455a638bedad1c7e0b023c5eba2368c8633943e
Raz <raz.chiriac@gmail.com>
2016-05-08 14:16:59 -0400

    firebase bindings

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,40 +1,45 @@
-app.controller('Dashboard', ['$rootScope', '$scope', '$firebaseObject', function ($rootScope, $scope, $firebaseObject) {
+app.controller('Dashboard', ['$rootScope', '$scope', '$firebaseObject', '$firebaseArray', '$mdDialog', function ($rootScope, $scope, $firebaseObject, $firebaseArray, $mdDialog) {
 
     $scope.init = init;
 
     var ref = new Firebase("https://raz-garden.firebaseio.com");
     $scope.plants = $firebaseArray(ref);
 
-    // add new items to the array
-    // the object is automatically added to our Firebase database!
-    $scope.addPlant = function() {
-        $scope.plants.$add({
-            Type: $scope.newPlantType,
-            Edible: $scope.newPlantEdible,
-            Title: $scope.newPlantTitle,
-            Height: $scope.newPlantHeight
+    $scope.showAdd = function (ev) {
+        $mdDialog.show({
+            controller: AddDialogController,
+            templateUrl: '/partials/templates/addDialog.tmpl.html',
+            targetEvent: ev,
+            clickOutsideToClose: true,
+            autoWrap: false,
+            hasBackdrop: false,
+            locals: {
+                plants: $scope.plants
+            }
         });
+        function AddDialogController($scope, plants) {
+            $scope.plants = plants;
+            // add new items to the array
+            // the object is automatically added to our Firebase database!
+            $scope.addPlant = function() {
+                $scope.plants.$add({
+                    Type: $scope.newPlantType,
+                    Edible: $scope.newPlantEdible,
+                    Title: $scope.newPlantTitle,
+                    Height: $scope.newPlantHeight
+                });
+                $scope.hide();
+            };
+            $scope.hide = function () {
+                $mdDialog.hide();
+            };
+        }
     };
-
-
 
 
     function init() {
         console.log('Hi Dashboard');
     }
-
-    $scope.plants = [
-        {
-            "Id": "0000",
-            "Type": "Veggie",
-            "Edible": true,
-            "Data": {
-                "Title": "Basil",
-                "Image": "images/basil.png",
-                "Height": "12" //inches
-            }
-        }
-    ];
 
 
 }]);
